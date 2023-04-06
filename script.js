d221bc58a25959d3e9c7c85cc28027ad36474037
Max <max@Maxs-MacBook-Pro.local>
2023-04-06 16:20:23 -0700

    update

--- script.js
+++ script.js
@@ -34,7 +34,7 @@ function generatePassword() {
   // generate random length between minLen and maxLen
   var passwordLen = Math.round(Math.random() * 10 + (maxLen - minLen))
   // random characters to be included in the password
-  const lowerCaseCharacters = 'akdkerurytmvnbxfertiyotirueyygdfscxbchgfgyttioypyitiuryeggdhfyadqewtcvzcxmnb'
+  const lowerCaseCharacters = 'abcdefghijklmnopqrstuvwxyz'
 
 
   // generate password
